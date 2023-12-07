50437cae285c59e25a30b3c74f8893310aac94bb
Tran Khanh Coder <91669973+felixwebdev@users.noreply.github.com>
2023-12-07 21:11:32 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,4 +15,62 @@ document.getElementById('prev').onclick = function(){
     refresh = setInterval(() =>{next.click()},10000)
 }
 
-  
+
+const signinBtns = document.querySelectorAll('.js-sign-in')
+const signinPopUp = document.querySelector('.sign-in-popup')
+const closeBtn = document.querySelector('.js-root-close')
+const noneClose = document.querySelector('.js-root-container')
+const signupBtns = document.querySelectorAll('.js-sign-up')
+const textSign = document.querySelector('.text-sign')
+const signBtn = document.querySelector('.sign-submit-btn')
+const passlost = document.querySelector('.passlost')
+const signContent = document.querySelector('.js-sign-content')
+
+
+function showUp() {
+    signinPopUp.classList.add('open')
+    textSign.innerHTML = "Bạn đã có tài khoản?"
+    signBtn.innerHTML = "Đăng nhập"
+    passlost.classList.add('remove')
+    signContent.innerHTML = "Chào mừng đến với FFOOD"
+ }
+
+ function showIn() {
+    signinPopUp.classList.add('open')
+    textSign.innerHTML = "Bạn chưa có tài khoản?"
+    signBtn.innerHTML = "Đăng kí"
+    passlost.classList.remove('remove')
+    signContent.innerHTML = "Đăng nhập vào FFOOD"
+ }
+
+ function ifsign() {
+    if (signBtn.innerHTML == "Đăng kí") {
+        showUp()
+    }
+    else {
+        showIn()
+    }
+ }
+
+signBtn.addEventListener('click',ifsign)
+
+function hidePopup() {
+   signinPopUp.classList.remove('open')
+}
+
+for (const signupBtn of signupBtns) {
+    signupBtn.addEventListener('click', showUp)
+}
+
+for (const signinBtn of signinBtns) {
+    signinBtn.addEventListener('click', showIn)
+}
+
+signinPopUp.addEventListener('click', hidePopup)
+closeBtn.addEventListener('click', hidePopup)
+noneClose.addEventListener('click', function(event) {
+    event.stopPropagation()
+})
+
+
+
